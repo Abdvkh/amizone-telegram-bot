d14e3cd219d915b05a94e1cf482147c21c7a3d9f
abdvkh <e-address@bk.ru>
2021-03-14 17:17:02 +0500

    Implement most of the features; Add update information; Introduce use of User class;

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+import importlib
+amizone_core = importlib.import_module("amizone-api.core")
+
+from core import User
+
 from rocketgram import Bot, Dispatcher, UpdatesExecutor, UpdateType, ChatType
-from rocketgram import context, commonfilters, commonwaiters
+from rocketgram import context, commonfilters
 from rocketgram import SendMessage
 from rocketgram import make_waiter
 
@@ -23,7 +28,10 @@ TRANSLATIONS = {
         'got_password': 'Your password is `{}`.',
         'logged': 'You are successfully logged in!',
         'information_updated': 'Your information was successfully updated!',
-        'under_construction': 'This section is under construction'
+        'under_construction': 'This section is under construction',
+        'went_wrong': 'Something went wrong!',
+        'information_update': 'Your information is updated successfully!',
+        'process': 'Being processed...'
     }
 }
 
@@ -49,30 +57,49 @@ def next():
 @router.handler
 @commonfilters.command('/login')
 async def auth():
+    user_telegram_id = context.user.user_id
+    user = User(user_telegram_id)
+
     await SendMessage(
-        context.user.user_id,
+        user_telegram_id,
         get_translation('enter_amizone_username')
     ).send()
     yield next()
 
     username = context.message.text
+    user.set_property('username', username)
+
     await SendMessage(
-        context.user.user_id,
+        user_telegram_id,
         get_translation('got_username').format(username)
     ).send()
 
     await SendMessage(
-        context.user.user_id,
+        user_telegram_id,
         get_translation('enter_amizone_password')
     ).send()
     yield next()
 
     password = context.message.text
+    user.set_property('password', password)
+
     await SendMessage(
-        context.user.user_id,
+        user_telegram_id,
         get_translation('got_password').format(password)
     ).send()
-    await SendMessage(context.user.user_id, get_translation('logged'))
+    await SendMessage(
+        user_telegram_id,
+        get_translation('got_password').format(password)
+    ).send()
+
+    login = amizone_core.Amizone(user.get_property('username'), user.get_property('password'))
+
+    if login:
+        msg = get_translation('logged')
+    else:
+        msg = get_translation('went_wrong')
+
+    await SendMessage(user_telegram_id, msg).send()
 
 
 @router.handler
@@ -82,33 +109,56 @@ async def help():
 
 
 @router.handler
-@commonfilters.command('/update')
-async def update():
-    await SendMessage(context.user.user_id, get_translation('under_construction')).send()
-
-
-@router.handler
 @commonfilters.command('/profile')
 async def profile():
-    await SendMessage(context.user.user_id, get_translation('under_construction')).send()
+    user_telegram_id = context.user.user_id
+    user = User(user_telegram_id)
+    amizone = amizone_core.Amizone(user.get_property('username'), user.get_property('password'))
+
+    profile_information = '\n'.join(amizone.get_profile().values())
+
+    await SendMessage(user_telegram_id, profile_information).send()
 
 
 @router.handler
 @commonfilters.command('/timetable')
 async def timetable():
-    await SendMessage(context.user.user_id, get_translation('under_construction')).send()
+    user_telegram_id = context.user.user_id
+    user = User(user_telegram_id)
+    amizone = amizone_core.Amizone(user.get_property('username'), user.get_property('password'))
+
+    timetable_information = amizone.get_timetable()
+    msg = '\n'.join([' | '.join(information.values()) for information in timetable_information])
+
+    await SendMessage(context.user.user_id, msg).send()
 
 
 @router.handler
 @commonfilters.command('/courses')
 async def courses():
-    await SendMessage(context.user.user_id, get_translation('under_construction')).send()
+    user_telegram_id = context.user.user_id
+    user = User(user_telegram_id)
+
+    amizone = amizone_core.Amizone(user.get_property('username'), user.get_property('password'))
+
+    courses_information = amizone.get_courses()
+    msg = '\n'.join([' | '.join(course_information.values()) for course_information in courses_information])
+
+    await SendMessage(context.user.user_id, msg).send()
 
 
 @router.handler
 @commonfilters.command('/faculties')
 async def faculties():
-    await SendMessage(context.user.user_id, get_translation('under_construction')).send()
+    user_telegram_id = context.user.user_id
+    user = User(user_telegram_id)
+
+    amizone = amizone_core.Amizone(user.get_property('username'), user.get_property('password'))
+
+    faculties_information = amizone.get_faculties()
+    msg = '\n'.join([' | '.join(faculty_information.values()) for faculty_information in faculties_information])
+
+    await SendMessage(context.user.user_id, msg).send()
 
 
 @router.handler
@@ -119,8 +169,28 @@ async def exam_results():
 
 @router.handler
 @commonfilters.command('/examSchedule')
-async def exam_schedule():
-    await SendMessage(context.user.user_id, get_translation('under_construction')).send()
+async def exams_schedule():
+    user_telegram_id = context.user.user_id
+    user = User(user_telegram_id)
 
+    amizone = amizone_core.Amizone(user.get_property('username'), user.get_property('password'))
+
+    exams_schedule_information = amizone.get_exam_schedule()
+    msg = '\n'.join([' | '.join(exam_schedule_information.values()) for exam_schedule_information in exams_schedule_information])
+
+    await SendMessage(context.user.user_id, msg).send()
+
+
+@router.handler
+@commonfilters.command('/update')
+async def update_information():
+    user_telegram_id = context.user.user_id
+    user = User(user_telegram_id)
+    amizone = amizone_core.Amizone(user.get_property('username'), user.get_property('password'))
+
+    await SendMessage(context.user.user_id, get_translation('process')).send()
+
+    amizone.update()
+    await SendMessage(context.user.user_id, get_translation('information_updated')).send()
 
 UpdatesExecutor.run(bot)
